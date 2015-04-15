d36a52ff06851c7696967e65b5c7b76ef1f53c88
Scott <scottcazan@gmail.com>
2015-04-15 00:01:50 -0700

    Modularized the engine into a separate npm package "taskengine"

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var fs = require('fs'),
 	_ = require('underscore'),
 	clc = require('cli-color'),
+	taskEngine = require('taskengine')(),
 	taskFile = process.env.HOME + "/tasks.json",
 	tasks,
 	command = process.argv[2],
@@ -23,7 +24,11 @@ var fs = require('fs'),
 	};
 
 var init = function init() {
-	fs.readFile(taskFile, 'utf8', readTasks);
+
+	taskEngine.init(function() {
+		executeCommands();
+		exit();
+	});
 };
 
 var executeCommands = function executeCommands() {
@@ -39,20 +44,20 @@ var executeCommands = function executeCommands() {
 
 		case "add":
 		case "a":
-			displayTask( addTask(passedData, process.argv[4]) );
+			displayTask( taskEngine.addTask(passedData, process.argv[4]) );
 			break;
 
 		case "rm":
-			displayTask( removeTask(passedData) );
+			displayTask( taskEngine.removeTask(passedData) );
 			break;
 
 		case "x":
 		case "close":
-			displayTask( closeTask(passedData) );
+			displayTask( taskEngine.closeTask(passedData) );
 			break;
 
 		case "edit":
-			var editedTask = editTask(passedData, process.argv[4]);
+			var editedTask = taskEngine.editTask(passedData, process.argv[4]);
 
 			if(editedTask) {
 				displayTask( editedTask );
@@ -63,7 +68,7 @@ var executeCommands = function executeCommands() {
 			break;
 
 		case "move":
-			moveTask();
+			taskEngine.moveTask();
 			break;
 
 		default:
@@ -73,7 +78,7 @@ var executeCommands = function executeCommands() {
 };
 
 var listAllTasks = function listAllTasks(passedArgs) {
-	var options = {collection: tasks};
+	var options = {collection: taskEngine.tasks};
 
 	if(passedArgs == "a" || passedArgs == "all") {
 		options.all = true;
@@ -91,38 +96,12 @@ var listAllTasks = function listAllTasks(passedArgs) {
  * @return {undefined}
  */
 var exit = function exit() {
-	writeTasks();
-	//taskLib.close();
+	taskEngine.close();
 };
 
-/**
- * Parse the tasks from a string and set the global tasks array
- *
- * @param err
- * @param {String} data
- * @return {undefined}
- */
-var readTasks = function readTasks(err, data) {
-	tasks = JSON.parse(data);
-
-	var sortedTasks = _.sortBy(tasks, function(task) { return parseInt(task.id, 10); });
-	largestID = sortedTasks[sortedTasks.length-1].id;
-
-	executeCommands();
-	exit();
-};
 
 /**
- * Write all tasks to the JSON file
- *
- * @return {undefined}
- */
-var writeTasks = function writeTasks() {
-	fs.writeFile(taskFile, JSON.stringify(tasks), function(){});
-};
-
-/**
- * Get all tasks
+ * Display all tasks
  *
  * @return {undefined}
  */
@@ -142,7 +121,7 @@ var displayTasks = function displayTasks(options) {
 		}
 	}
 	else if(taskID) {
-		taskList = getTaskByID( taskID );
+		taskList = taskEngine.getTaskByID( taskID );
 
 		if(taskList) {
 			process.stdout.write(clc.redBright("\n" + taskList.name + "\n\n") );
@@ -154,7 +133,7 @@ var displayTasks = function displayTasks(options) {
 
 		for(var i=0; i<taskList.length; i++) {
 			var taskID = taskList[i];
-			var task = getTaskByID(taskID);
+			var task = taskEngine.getTaskByID(taskID);
 			task && displayTask(task);
 		}
 	}
@@ -197,146 +176,6 @@ var displayTask = function displayTask(task) {
 		task.description && process.stdout.write(clc.blackBright("	" + task.description) + "\n");
 	}
 	process.stdout.write("\n");
-};
-
-var getTaskByID = function getTaskByID(taskID) {
-	var task = _.findWhere(tasks, {id: taskID});
-
-	return task;
-};
-
-/**
- * Parse the "key: value" formatted input into an object
- *
- * @param {String} data The input string
- * @return {object} The parsed object
- */
-var parseInputData = function parseInputData(data) {
-	var params = data.split(","),
-		parsedObject = {};
-
-	for(var i=params.length-1; i >=0; i--) {
-		var keyValue = params[i].split(":");
-
-		parsedObject[ keyValue[0].split(' ').join('') ] = keyValue[1];
-	}
-
-	return parsedObject;
-
-};
-
-/**
- * Add the given task to our array
- *
- * @param taskData
- * @param callback
- * @return {undefined}
- */
-var addTask = function addTask(taskData, subTaskData) {
-	var subTaskList,
-		parsedData,
-		subTask = false;
-
-	if(taskData !== undefined) {
-		if(parseInt(taskData,10) > -1) {
-			subTaskList = getTaskByID(parseInt(taskData,10)).subTasks;
-
-			// If we passed a number instead of data, use the 5th argument and add it as a subtask
-			parsedData = parseInputData(subTaskData);
-			subTask = true;
-		}
-		else {
-			parsedData = parseInputData(taskData);
-		}
-
-
-		var newTask = _.clone(defaultTask)
-		_.extend(newTask, parsedData);
-
-		newTask.dateAdded = Date.now();
-		newTask.id = largestID + 1;
-		newTask.subTask = subTask;
-
-		tasks.unshift( newTask );
-
-		if(subTask) {
-			subTaskList.push(newTask.id);
-		}
-
-		console.log('Added:');
-		return newTask;
-	}
-	else {
-		console.log('no task name given');
-		return false;
-	}
-
-};
-
-var editTask = function editTask(taskID, params) {
-	taskID = parseInt( taskID, 10 );
-	params = parseInputData(params);
-
-	var task = getTaskByID(taskID);
-
-	// Concatenate name or description if indicated
-	if(params.description && params.description[0] === "+") {
-		params.description = task.description + " " + params.description;
-	}
-	if(params.name && params.name[0] === "+") {
-		params.name = task.name + " " + params.name;
-	}
-
-	if(task) {
-		_.extend(task, params);
-
-		return task;
-	}
-	else {
-		return false;
-	}
-};
-
-var moveTask = function() {
-
-};
-
-var removeTask = function removeTask(taskID) {
-	taskID = parseInt(taskID, 10);
-	var task = getTaskByID(taskID);
-	var taskIndex = tasks.indexOf(task);
-
-	if(task) {
-		tasks.splice(taskIndex, 1);
-
-		console.log('Removing:');
-		return task;
-	}
-	else {
-		return false;
-	}
-
-};
-
-/**
- * Set a task as "done" or "closed"
- *
- * @return {undefined}
- */
-var closeTask = function closeTask(taskID) {
-	taskID = parseInt(taskID, 10);
-	var task = getTaskByID(taskID);
-
-	if(task) {
-		task.open = false;
-		console.log('closing:');
-
-		return task;
-	}
-	else {
-		return false;
-	}
-
 };
 
 // Start the program
